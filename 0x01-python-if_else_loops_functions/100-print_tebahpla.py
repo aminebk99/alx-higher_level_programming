93875062b669a6f51c7a8e7f1a830a3737d2445b
amine <mohamedaminebk99@gmailcom>
2024-01-02 10:13:29 -0800

    commit

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123, 2):
-    print(f"[i[-1]]", end=" ")
+for i in range(122, 96, -1):
+    if i % 2 != 0:
+        i = i - 32
+    print("{}".format(chr(i)), end='')
