5ad0cb5854fd94b091aa1797af32e46e719b73b9
amine <mohamedaminebk99@gmailcom>
2024-01-11 16:13:15 -0800

    commit

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string or not isinstance(roman_string, str):
+        return 0
+
     roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
                   'C': 100, 'D': 500, 'M': 1000}
 
@@ -14,4 +17,4 @@ def roman_to_int(roman_string):
             result += value
         prev_value = value
 
-    return (result)
+    return result
