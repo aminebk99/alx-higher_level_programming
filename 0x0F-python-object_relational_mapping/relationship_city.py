eaed232c3c54d33ad046c5a732dd2b246988d9ea
Amine bazaou <mohamedaminebk99@gmail.com>
2024-04-14 14:29:29 -0700

    advanced

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 from sqlalchemy import Column, Integer, String, text, ForeignKey
 from relationship_state import Base
-from sqlalchemy.orm import relationship
 """
     Module that performs creates a States class based off of Base.
 """
@@ -15,4 +14,3 @@ class City(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    state = relationship("State", backref="cities")
